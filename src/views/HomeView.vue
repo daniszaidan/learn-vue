a54525156e698d82e9de8a1d18298b5d0c56b299
Danis Zaidan <daniszaidan@gmail.com>
2025-06-22 22:55:28 +0700

    different about script setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import TheWelcome from '../components/TheWelcome.vue'
+
+let message = ref('Hello hello')
+setTimeout(() => {
+  message.value = 'change hello'
+}, 5000)
 </script>
 
 <template>
   <main>
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" />
+
     <TheWelcome />
   </main>
 </template>
