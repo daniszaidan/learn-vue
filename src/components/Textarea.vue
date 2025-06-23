05286553b666061216fdda24339470346875fe20
Danis Zaidan <daniszaidan@gmail.com>
2025-06-23 15:12:24 +0700

    reusable component with typing handler

---
+++ src/components/Textarea.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  modelValue: String,
+})
+
+let emit = defineEmits(['update:modelValue'])
+
+const onTabPress = (event) => {
+  let textarea = event.target
+  let val = textarea.value
+  let start = textarea.selectionStart
+  let end = textarea.selectionEnd
+
+  textarea.value = val.substring(0, start) + '\t' + val.substring(end)
+
+  textarea.selectionStart = textarea.selectionEnd = start + 1
+}
+
+const update = (event) => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+  <textarea @keydown.tab.prevent="onTabPress" @keyup="update" v-text="modelValue" />
+</template>
+
+<style>
+textarea {
+  width: 100%;
+  height: 200px;
+  margin-top: 50px;
+}
+</style>
---
+++ src/views/FormView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Textarea from '@/components/Textarea.vue'
+import { ref } from 'vue'
+
+let comment = ref('test value')
+</script>
+
+<template>
+  <form>
+    <Textarea v-model:comment="comment" />
+  </form>
+</template>
